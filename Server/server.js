33d074ecfc858b427b33e646034fc94789994204
Mingc <li.mingchen21@gmail.com>
2022-07-11 15:47:52 -0400

    m

--- Server/server.js
+++ Server/server.js
@@ -122,10 +122,13 @@ app.all( "*", ( req, res ) => {
 } );
 
 app.use(express.static(path.join(__dirname, '../build')));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
-  });
-  
+
+if ( process.env.NODE_ENV !== "production" ) {
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../build', 'index.html'));
+      });
+}
+
 app.use( ( err, req, res, next ) => {
     // error handler
     console.error( err );
